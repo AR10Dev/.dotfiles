5ce82f942a09be18d7c555bdb785ddae366e3942
Avaab Razzaq <40202832+AR10Dev@users.noreply.github.com>
2023-04-19 01:09:14 +0200

    Add sudo

--- install.sh
+++ install.sh
@@ -7,8 +7,8 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 #==============
 # Delete existing dot files and folders
 #==============
-rm -rf ~/.zshrc > /dev/null 2>&1
-rm -rf ~/.config > /dev/null 2>&1
+sudo rm -rf ~/.zshrc > /dev/null 2>&1
+sudo rm -rf ~/.config > /dev/null 2>&1
 
 #==============
 # Create symlinks in the home folder
@@ -19,4 +19,4 @@ ln -sf $SCRIPTPATH/.config ~/.config
 #==============
 # Set zsh as the default shell
 #==============
-chsh -s /bin/zsh
+sudo chsh -s /bin/zsh
